97aaa1ed0a27dbe7f06ee413efcfebd5908d3371
Tom Coakes <tomcoakes@gmail.com>
2015-11-20 15:41:38 +0000

    Send list of articles back to slack

--- routes/search.js
+++ routes/search.js
@@ -4,12 +4,49 @@ module.exports = function (server) {
 
   server.post('/search/', function (req, res, next) {
 
-    request('http://www.google.com', function (error, response, body) {
+    var options = {
+      url: 'http://localhost:11040/search/more?startFrom=0&q=' + req.body.text,
+      headers: {
+        'Accept': 'application/json'
+      }
+    };
+
+    request(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log(body) // Show the HTML for the Google homepage.
+        res.setHeader('Content-Type', 'application/json');
+        var parsedBody = JSON.parse(body);
+
+
+
+        var attachments = parsedBody.searchResults.hits.map(function(intel){
+          return {
+            "title": intel.headline,
+            "title_link": intel.url,
+            "text": intel.body,
+            "fields": [
+              {
+                "title": "Country",
+                "value": intel.dominantCountry,
+                "short": true
+              },
+              {
+                "title": "Sector",
+                "value": intel.dominantSector,
+                "short": true
+              }
+            ],
+            "color": "#002344"
+          };
+        });
+
+        var output = {
+          "text": "Search Results for: " + req.body.text + " (" + parsedBody.searchResults.hits.length + " results found)",
+          "attachments": attachments
+        }
+        res.send(output);
       }
     })
 
-    res.send("You've made a search for: " + req.body.text);
+
   });
 };
