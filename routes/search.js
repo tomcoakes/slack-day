7678f11775827bcd7fcba3de5382af1174ae42e9
Tom Coakes <tomcoakes@gmail.com>
2015-11-20 14:14:25 +0000

    Added request

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,15 @@
+var request = require('request');
+
 module.exports = function (server) {
 
   server.post('/search/', function (req, res, next) {
-    console.log(req.body.text);
-    res.send("You've made a search!");
+
+    request('http://www.google.com', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body) // Show the HTML for the Google homepage.
+      }
+    })
+
+    res.send("You've made a search for: " + req.body.text);
   });
 };
